fc80a3f46b6b1f2e1785cb66a1c86e100290f16e
Ernesto GM <ernt26dev@gmail.com>
2016-09-22 09:00:51 -0500

    Modifica atributos y métodos de la clase Conexion

--- src/datos/Conexion.java
+++ src/datos/Conexion.java
@@ -2,20 +2,32 @@ package datos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
-    private String driver = "com.mysql.jdbc.Driver";
-    private String connectString = "jdbc:mysql://192.168.100.7:3306/dbCaduct";
-    private String user="uAdminProyectos";
-    private String password="12345";
-    public Connection con;
-        
-    public Conexion(){
+
+    private final String user = "uAdminProyectos";
+    private final String password = "12345";
+    private final String host = "192.168.1.66";
+    private final String bd = "caductBD";
+    private final String url = "jdbc:mysql://" + host + "/" + bd;
+    private Connection con = null;
+
+    public Conexion() {
         try {
-            Class.forName(driver);
-            con = DriverManager.getConnection(connectString, user , password);
-        }catch ( Exception e ){
-            System.out.println("Error: No se pudo conectar a la base de datos: "+e.getMessage());
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            System.out.println("No se pudo conectar a la base de datos: " + ex.getMessage());
+        }
+    }
+
+    public Connection getConexion() {
+        return con;
+    }
+
+    public void cerrarConexion() throws SQLException {
+        if (con != null) {
+            con.close();
         }
     }
 }
